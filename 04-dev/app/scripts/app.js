452c511725195f8b2bd08fecb516586d2ad6f052
Nicola Deufemia <ndeufemia.mmt-b2014@fh-salzburg.ac.at>
2016-02-22 20:17:25 +0100

    developed in wrong folder, now added to project

--- 04-dev/app/scripts/app.js
+++ 04-dev/app/scripts/app.js
@@ -1,8 +1,16 @@
 import $ from 'jquery'
 import page from 'page'
+import Handlebars from 'hbsfy/runtime'
 import * as pages from './pages'
+import dateFormat from './helpers/date-format'
+import times from 'handlebars-helper-repeat'
+import eq from './helpers/eq'
 
 const $nav = $('#nav')
+
+Handlebars.registerHelper('dateFormat', dateFormat)
+Handlebars.registerHelper('times', times)
+Handlebars.registerHelper('eq', eq)
 
 page('*', function(ctx, next) {
   $nav
@@ -18,7 +26,10 @@ page('*', function(ctx, next) {
 page('/', '/home')
 page('/home', pages.home)
 page('/contact', pages.contact)
+page('/drivers', pages.drivers)
+page('/drivers/:driver', pages.driver)
 
+page('/error', pages.internalError)
 page('*', pages.notFound)
 
 page()
--- 04-dev/app/scripts/pages/index.js
+++ 04-dev/app/scripts/pages/index.js
@@ -2,12 +2,16 @@ import $ from 'jquery'
 import page from 'page'
 import fetch from 'isomorphic-fetch'
 import url from 'url'
+import config from '../config'
 import tplHome from '../templates/home.hbs'
 import tplContact from '../templates/contact.hbs'
+import tplDrivers from '../templates/drivers.hbs'
+import tplDriver from '../templates/driver.hbs'
 import tplNotFound from '../templates/not-found.hbs'
+import tplError from '../templates/error.hbs'
 
 const $content = $('#content')
-const limit = 30
+const limit = 25
 let globalError
 
 export function home() {
@@ -22,4 +26,66 @@ export function notFound() {
 export function contact() {
   $content.html(tplContact())
 }
+
+export function drivers(ctx) {
+  const u = url.parse(ctx.path, true)
+  const currentPage = Math.max(1, u.query.page ? u.query.page : 1)
+
+  fetch(config.api.url + `/drivers.json?limit=${limit}&offset=${limit * (currentPage - 1)}`)
+
+  .then(response => {
+      if (response.status >= 400) {
+      return currentPage('error')
+    }
+  return response.json()
+  })
+
+  .then(data => {
+      const totalPages = Math.ceil(data.MRData.total / data.MRData.limit)
+      $content.html(
+        tplDrivers({
+          from: currentPage > 3 ? currentPage - 3 : 1,
+          to: currentPage < totalPages - 4 ? totalPages - 4 : totalPages,
+          total: totalPages,
+          current: currentPage,
+          prev: currentPage > 1 ? currentPage - 1 : false,
+          next: currentPage < totalPages ? currentPage + 1 : false,
+          drivers: data.MRData.DriverTable.Drivers
+        }))
+  })
+
+  .catch(err => {
+      globalError = err
+    page('/error')
+  })
+}
+
+export function driver(ctx) {
+  fetch(config.api.url + `/drivers/${ctx.params.driver}.json`)
+  .then(response => {
+        if (response.status >= 400) {
+            return page('error')
+        }
+        return response.json()
+  })
+
+  .then(data => {
+    $content.html(
+      tplDriver({
+          driver: data.MRData.DriverTable.Drivers
+      }))
+  })
+
+  .catch(err => {
+    globalError = err
+    page('/error')
+  })
+}
+
+export function internalError() {
+    $content.html(tplError({error: globalError}))
+    throw globalError
+    globalError = undefined
+}
+
 
